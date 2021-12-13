3ad09412759a29c907d859afb1bb0c86bed7efa6
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2021-12-13 21:22:00 +0530

    JavaScript Code

--- DHTML/employeeDetails/dynamicEventHandling.js
+++ DHTML/employeeDetails/dynamicEventHandling.js
@@ -1,13 +1,19 @@
 var validateData1 = (event, clas1) => {
-    if(event.charCode >= 48 && event.charCode <= 57) {
+    console.log(document.querySelector("#gender").value);
+    /*if(document.querySelector("#gender").value !== "male" || document.querySelector("#gender").value !== "female") {
+        document.querySelector(".genderError").style.display = 'block';
+        return false;
+    }
+    else */if(event.charCode >= 48 && event.charCode <= 57) {
         document.querySelector(clas1).style.display = 'none';
         return true;
     }
-    document.querySelector(clas1).style.display = 'block';
-    return false;
+    else {
+        document.querySelector(clas1).style.display = 'block';
+        return false;
+    }
 }
 var validateData2 = (event) => {
-    console.log(document.querySelector("#age").value);
     if(document.querySelector("#age").value <= 18) {
         document.querySelector(".minAge").style.display = 'block';
         return false;
